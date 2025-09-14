c6837683ef57ac125802d06e985f5350c928f607
Vitaly Efremov <dergraff@gmail.com>
2025-09-14 11:14:14 +1000

    Валидация точки добавления книги с помощью FluentValidation

---
+++ Library.Api/Validators/BookValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Library.Api.Models;
+
+namespace Library.Api.Validators;
+
+public class BookValidator : AbstractValidator<Book>
+{
+    public BookValidator()
+    {
+        RuleFor(book => book.Isnb)
+            .Matches(@"^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$")
+            .WithMessage("Value was not a valid ISBN number");
+        
+        RuleFor(book => book.Title).NotEmpty();
+        RuleFor(book => book.Description).NotEmpty();
+        RuleFor(book => book.PageCount).NotEmpty();
+        RuleFor(book => book.Author).NotEmpty();
+    }
+}
